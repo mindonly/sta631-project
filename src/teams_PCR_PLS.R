12943c87ef9f60bdf25ad5a33231775acc6f54fc
mindonly <thesanch@gmail.com>
2018-12-08 14:15:17 -0500

    new RD, PCR, and PLS code

---
+++ src/teams_PCR_PLS.R
@@ -0,0 +1,153 @@
+library(car)
+library(pls)
+
+# BEGIN DATA INPUT #
+#
+    # reproducibility
+set.seed(42)
+    # Lahman's baseball databank
+setwd("~/Dropbox/f2018/sta631/Project/baseballdatabank-master/core/")
+    # intentionally using base not 'readr' to input CSV; HBP and SF columns mis-handled by 'readr'
+teams <- read.csv("Teams.csv")
+    # drop any winless teams: e.g. 1872 NAT & 1873 MAR
+    # keeping them in breaks lm(logWratio) call below
+teams <- subset(teams, W != 0)
+
+    # SUBSET DATA SINCE 1970
+teams_1970 <- subset(teams, yearID > 1969)[ , c(01, 04, 07, 09, 10, 15:41)]
+   # derive run-diff, win-pct, and singles (X1B) columns
+teams_1970$RD <- with(teams_1970, R - RA)
+teams_1970$Wpct <- with(teams_1970, W / (W + L))
+teams_1970$X1B <- with(teams_1970, H - (X2B + X3B + HR))
+   # deriv on-base percent (OBP), slugging percent (SLG), and OPS (on-base + slugging)
+   # only possible for data since 1970 due to HBP and SF
+teams_1970$OBP <- with(teams_1970, (H + BB + HBP) / (AB + BB + HBP +SF) )
+#teams_1970$SLG <- with(teams_1970, ( (H - (X2B + X3B + HR) + (2*X2B) + (3*X3B) + (4*HR)) ) / AB)
+teams_1970$SLG <- with(teams_1970, (X1B + (2*X2B) + (3*X3B) + (4*HR)) / AB)
+teams_1970$OPS <- with(teams_1970, OBP + SLG)
+   # train/test
+train_1970 <- sample(1:nrow(teams_1970), nrow(teams_1970) * 0.6667)
+test_1970 <- (-train_1970)
+#
+# END DATA INPUT #
+
+
+# PCR and PLS #
+#
+    # drop non-predictors
+    # subset select option, by row below:
+    # 1. derived: run-diff, win-pct
+    # 2. batting/scoring
+    # 3. pitching
+    # 4. fielding
+    # NOTE: SO, CS, SB removed due to lack of complete cases
+    # also removed R, RA, H, OPS, and IPouts
+
+# (with RD)
+slim_1970 <- subset(teams_1970, 
+                    select = c(RD, Wpct, 
+                               AB, X1B, X2B, X3B, HR, BB, OBP, SLG,
+                               ER, ERA, CG, SHO, SV, HA, HRA, BBA, SOA, 
+                               E, DP, FP))
+# (without RD)
+slim_1970_noRD <- subset(teams_1970, 
+                    select = c(Wpct, 
+                               AB, X1B, X2B, X3B, HR, BB, OBP, SLG,
+                               ER, ERA, CG, SHO, SV, HA, HRA, BBA, SOA, 
+                               E, DP, FP))
+
+# PCR
+#
+# 1970 PCR with RD #
+    # set up model matrix
+x <- model.matrix(Wpct ~ ., slim_1970)[, -1]
+y <- slim_1970$Wpct
+y.test <- y[test_1970]
+    # full dataset
+pcr.fit.full <- pcr(Wpct ~ ., data=slim_1970, scale=TRUE, validation="CV")
+summary(pcr.fit.full)
+validationplot(pcr.fit.full, val.type="MSEP")
+    # train/test
+pcr.fit.train <- pcr(Wpct ~ ., data=slim_1970, subset=train_1970, scale=TRUE, validation="CV")
+summary(pcr.fit.train)
+validationplot(pcr.fit.train, val.type="MSEP")
+    # fit on full dataset, get RMSE
+pcr.pred <- predict(pcr.fit.train, x[test_1970, ], ncomp=4)
+sqrt(mean((pcr.pred - y.test)^2))
+    # loading matrices
+loadings(pcr.fit.full)[, 1:6]
+abs(loadings(pcr.fit.full)[, 1:6]) > 0.30
+loadings(pcr.fit.train)[, 1:6]
+abs(loadings(pcr.fit.train)[, 1:6]) > 0.30
+
+# 1970 PCR without RD
+    # set up model matrix (no RD)
+x <- model.matrix(Wpct ~ ., slim_1970_noRD)[, -1]
+y <- slim_1970_noRD$Wpct
+y.test <- y[test_1970]
+   # full dataset
+pcr.fit.full <- pcr(Wpct ~ ., data=slim_1970_noRD, scale=TRUE, validation="CV")
+summary(pcr.fit.full)
+validationplot(pcr.fit.full, val.type="MSEP")
+    # train/test
+pcr.fit.train <- pcr(Wpct ~ ., data=slim_1970_noRD, subset=train_1970, scale=TRUE, validation="CV")
+summary(pcr.fit.train)
+validationplot(pcr.fit.train, val.type="MSEP")
+    # fit on full dataset, get MSE
+pcr.pred <- predict(pcr.fit.train, x[test_1970, ], ncomp=4)
+sqrt(mean((pcr.pred - y.test)^2))
+    # loading matrices
+loadings(pcr.fit.full)[, 1:6]
+abs(loadings(pcr.fit.full)[, 1:6]) > 0.30
+loadings(pcr.fit.train)[, 1:6]
+abs(loadings(pcr.fit.train)[, 1:6]) > 0.30
+
+
+# PLS
+#
+# 1970 PLS with RD #
+   # set up model matrix
+x <- model.matrix(Wpct ~ ., slim_1970)[, -1]
+y <- slim_1970$Wpct
+y.test <- y[test_1970]
+   # full dataset
+pls.fit.full <- plsr(Wpct ~ ., data=slim_1970, scale=TRUE, validation="CV")
+summary(pls.fit.full)
+validationplot(pls.fit.full, val.type="MSEP")
+   # train/test
+pls.fit.train <- plsr(Wpct ~ ., data=slim_1970, subset=train_1970, scale=TRUE, validation="CV")
+summary(pls.fit.train)
+validationplot(pls.fit.train, val.type="MSEP")
+   # fit on full dataset, get RMSE
+pls.pred <- predict(pls.fit.train, x[test_1970, ], ncomp=3)
+sqrt(mean((pls.pred - y.test)^2))
+   # loading matrices
+loadings(pls.fit.full)[, 1:6]
+abs(loadings(pls.fit.full)[, 1:6]) > 0.35
+loadings(pls.fit.train)[, 1:6]
+abs(loadings(pls.fit.train)[, 1:6]) > 0.35
+
+# 1970 PLS without RD #
+   # set up model matrix (no RD)
+x <- model.matrix(Wpct ~ ., slim_1970_noRD)[, -1]
+y <- slim_1970_noRD$Wpct
+y.test <- y[test_1970]
+   # full dataset
+pls.fit.full <- plsr(Wpct ~ ., data=slim_1970_noRD, scale=TRUE, validation="CV")
+summary(pls.fit.full)
+validationplot(pls.fit.full, val.type="MSEP")
+   # train/test
+pls.fit.train <- plsr(Wpct ~ ., data=slim_1970_noRD, subset=train_1970, scale=TRUE, validation="CV")
+summary(pls.fit.train)
+validationplot(pls.fit.train, val.type="MSEP")
+   # fit on full dataset, get RMSE
+pls.pred <- predict(pls.fit.train, x[test_1970, ], ncomp=2)
+sqrt(mean((pls.pred - y.test)^2))
+   # loading matrices
+loadings(pls.fit.full)[, 1:6]
+abs(loadings(pls.fit.full)[, 1:6]) > 0.35
+loadings(pls.fit.train)[, 1:6]
+abs(loadings(pls.fit.train)[, 1:6]) > 0.35
+
+#
+# END PCR and PLS #
---
+++ src/teams_run_diff.R
@@ -0,0 +1,116 @@
+library(car)
+
+# BEGIN DATA INPUT #
+#
+# reproducibility
+set.seed(42)
+# Lahman's baseball databank
+setwd("~/Dropbox/f2018/sta631/Project/baseballdatabank-master/core/")
+# intentionally using base not 'readr' to input CSV; HBP and SF columns mis-handled by 'readr'
+teams <- read.csv("Teams.csv")
+# drop any winless teams: e.g. 1872 NAT & 1873 MAR
+# keeping them in breaks lm(logWratio) call below
+teams <- subset(teams, W != 0)
+# ALL DATA SINCE 1871
+teams_1871 <- subset(teams)[ , c(01, 04, 07, 09, 10, 15:41)]
+   # derive run-diff, win-pct, and singles (X1B) columns
+teams_1871$RD <- with(teams_1871, R - RA)
+teams_1871$Wpct <- with(teams_1871, W / (W + L))
+teams_1871$X1B <- with(teams_1871, H - (X2B + X3B + HR))
+   # train/test
+train_1871 <- sample(1:nrow(teams_1871), nrow(teams_1871) * 0.6667)
+test_1871 <- (-train_1871)
+# DATA SINCE 1970
+teams_1970 <- subset(teams, yearID > 1969)[ , c(01, 04, 07, 09, 10, 15:41)]
+   # derive run-diff, win-pct, and singles (X1B) columns
+teams_1970$RD <- with(teams_1970, R - RA)
+teams_1970$Wpct <- with(teams_1970, W / (W + L))
+teams_1970$X1B <- with(teams_1970, H - (X2B + X3B + HR))
+   # train/test
+train_1970 <- sample(1:nrow(teams_1970), nrow(teams_1970) * 0.6667)
+test_1970 <- (-train_1970)
+
+   # response vectors
+y_1871 = teams_1871$Wpct
+y_1970 = teams_1970$Wpct
+#
+# END DATA INPUT #
+
+
+# RUN DIFFERENTIAL
+
+   # ALL DATA since 1871
+
+   # plot and line of best fit for Wpct ~ RD (naive, full dataset)
+plot(teams_1871$RD, teams_1871$Wpct,
+     xlab="run differential",
+     ylab="winning percentage",
+     main="Win.Pct vs. Run.Diff, since 1871")
+rd_1871_naive.fit <- lm(Wpct ~ RD, data=teams_1871)
+summary(rd_1871_naive.fit)
+abline(a=coef(rd_1871_naive.fit)[1], b=coef(rd_1871_naive.fit)[2], lwd=2, col="red")
+   # predictions and residuals (naive, full dataset)
+teams_1871$linWpct <- predict(rd_1871_naive.fit)
+teams_1871$linResiduals <- residuals(rd_1871_naive.fit)
+plot(teams_1871$RD, teams_1871$linResiduals,
+     xlab="run differential",
+     ylab="residual",
+     xlim=c(-500, 500),
+     ylim=c(-0.25, 0.25),
+     main="Residuals vs. Run.Diff, Naive, since 1871")
+abline(h=0, lty=4)
+rd_1871_naive.linRMSE <- sqrt(mean(teams_1871$linResiduals^2))
+rd_1871_naive.linRMSE
+nrow(subset(teams_1871, abs(linResiduals) < rd_1871_naive.linRMSE)) / nrow(teams_1871)
+nrow(subset(teams_1871, abs(linResiduals) < 2 * rd_1871_naive.linRMSE)) / nrow(teams_1871)
+
+   # test/train linear fit on RD
+rd_1871_train.fit <- lm(Wpct ~ RD, data=teams_1871[train_1871, ])
+summary(rd_1871_train.fit)
+rd_1871_test.pred <- predict(rd_1871_train.fit, newdata=teams_1871[test_1871, ])
+rd_1871_test.linRMSE <- sqrt(mean((rd_1871_test.pred - y_1871[test_1871])^2))
+rd_1871_test.linRMSE 
+nrow(subset(teams_1871[test_1871, ], abs(linResiduals) < rd_1871_test.linRMSE)) / 
+   nrow(teams_1871[test_1871, ])
+nrow(subset(teams_1871[test_1871, ], abs(linResiduals) < 2 * rd_1871_test.linRMSE)) / 
+   nrow(teams_1871[test_1871, ])
+
+
+
+
+   # ONLY DATA since 1970
+
+# plot and line of best fit for Wpct ~ RD
+plot(teams_1970$RD, teams_1970$Wpct,
+     xlab="run differential",
+     ylab="winning percentage",
+     main="Win.Pct vs. Run.Diff, since 1970")
+rd_1970_naive.fit <- lm(Wpct ~ RD, data=teams_1970)
+rd_1970_naive.fit
+abline(a=coef(rd_1970_naive.fit)[1], b=coef(rd_1871_naive.fit)[2], lwd=2, col="red")
+   # predictions and residuals
+teams_1970$linWpct <- predict(rd_1970_naive.fit)
+teams_1970$linResiduals <- residuals(rd_1970_naive.fit)
+plot(teams_1970$RD, teams_1970$linResiduals,
+     xlab="run differential",
+     ylab="residual",
+     xlim=c(-400, 400),
+     ylim=c(-0.1, 0.1),
+     main="Residuals vs. Run.Diff, Naive, since 1970")
+abline(h=0, lty=4)
+rd_1970_naive.linRMSE <- sqrt(mean(teams_1970$linResiduals^2))
+rd_1970_naive.linRMSE
+nrow(subset(teams_1970, abs(linResiduals) < rd_1970_naive.linRMSE)) / nrow(teams_1970)
+nrow(subset(teams_1970, abs(linResiduals) < 2 * rd_1970_naive.linRMSE)) / nrow(teams_1970)
+
+   # test/train linear fit on RD
+rd_1970_train.fit <- lm(Wpct ~ RD, data=teams_1970[train_1970, ])
+summary(rd_1970_train.fit)
+rd_1970_test.pred <- predict(rd_1970_train.fit, newdata=teams_1970[test_1970, ])
+rd_1970_test.linRMSE <- sqrt(mean((rd_1970_test.pred - y_1970[test_1970])^2))
+rd_1970_test.linRMSE 
+nrow(subset(teams_1970[test_1970, ], abs(linResiduals) < rd_1970_test.linRMSE)) / 
+   nrow(teams_1970[test_1970, ])
+nrow(subset(teams_1970[test_1970, ], abs(linResiduals) < 2 * rd_1970_test.linRMSE)) / 
+   nrow(teams_1970[test_1970, ])
+
